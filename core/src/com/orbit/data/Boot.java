d602205dac15ba177b9015a4119e9f80b5cba0d0
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2017-05-22 17:04:57 -0500

    Set window settings

--- core/src/com/orbit/data/Boot.java
+++ core/src/com/orbit/data/Boot.java
@@ -14,6 +14,10 @@ public class Boot extends Game {
 
 	@Override
 	public void create () {
+		Gdx.graphics.setResizable(false);
+		Gdx.graphics.setWindowedMode(800,600);
+		Gdx.graphics.setTitle("Orbit Simulator");
+
 		manager = new AssetManager();
 
 		setScreen(new GameScreen(this));
