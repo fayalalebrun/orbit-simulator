9bdb1ea697749dd8ec882d78d15598097466f830
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2017-05-22 16:51:22 -0500

    Added assetmanager to boot

--- core/src/com/orbit/data/Boot.java
+++ core/src/com/orbit/data/Boot.java
@@ -3,22 +3,23 @@ package com.orbit.data;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Boot extends Game {
-	
+
+	AssetManager manager;
+
 	@Override
 	public void create () {
-
-
-	}
-
-
-	
-	@Override
-	public void dispose () {
+		manager = new AssetManager();
 
 	}
+
+	public AssetManager getManager(){
+		return manager;
+	}
+
 }
