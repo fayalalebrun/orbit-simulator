834ae48fb38b99733c136a217c5358c0118965e5
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2017-05-22 17:30:51 -0500

    Clear screen before drawing

--- core/src/com/orbit/data/GameScreen.java
+++ core/src/com/orbit/data/GameScreen.java
@@ -1,5 +1,7 @@
 package com.orbit.data;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -23,6 +25,8 @@ public class GameScreen extends BaseScreen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         stage.draw();
     }
 
