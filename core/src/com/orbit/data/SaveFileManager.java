de6d2e41581af9ca5c9557607d0d3f2946b92168
AYALA LEBRUN, FRANCISCO <fraayala19@francislalebrun.asfgdmn.local>
2017-05-31 17:59:28 -0500

    Implemented loading of systems

--- core/src/com/orbit/data/SaveFileManager.java
+++ core/src/com/orbit/data/SaveFileManager.java
@@ -2,7 +2,9 @@ package com.orbit.data;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
 import com.orbit.data.entities.Planet;
+import net.dermetfan.utils.StringUtils;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -15,7 +17,48 @@ import java.util.ArrayList;
 public class SaveFileManager {
 
     public static void loadGame(GameScreen gameScreen, FileHandle file){
+        String name;
+        double radius;
+        double mass;
+        double speed;
+        double angle;
+        Color c;
+        double x;
+        double y;
 
+        gameScreen.removePlanets(gameScreen.getPlanetArrayList());
+
+        String str = file.readString();
+        String part[] = str.replaceAll("\\r", "").split("\\n");
+        int i=0;
+        System.out.println(part.length);
+        while(i<part.length-2){
+            System.out.println(part[i].substring(0,4));
+            if(part[i].substring(0,4).equals("name")){
+                name = part[i].substring(6);
+                i++;
+                radius = Double.parseDouble(part[i].substring(8));
+                i++;
+                mass = Double.parseDouble(part[i].substring(6));
+                i++;
+                speed = Double.parseDouble(part[i].substring(7));
+                i++;
+                angle = Double.parseDouble(part[i].substring(7));
+                i++;
+                c = new Color(Integer.parseInt(part[i].substring(7)));
+                i++;
+                x = Double.parseDouble(part[i].substring(3));
+                i++;
+                y = Double.parseDouble(part[i].substring(3));
+                i++;
+
+                Planet p = new Planet(name, radius, mass, speed, angle, c, x, y, gameScreen.getPlanetArrayList());
+
+                gameScreen.addPlanet(p);
+            } else{
+                i++;
+            }
+        }
     }
 
     public static void saveGame(GameScreen gameScreen, FileHandle file){
@@ -23,7 +66,7 @@ public class SaveFileManager {
 
         Writer writer = file.writer(false);
         ArrayList<Planet> planetList = gameScreen.getPlanetArrayList();
-        
+
         for(Planet p: planetList){
             try {
                 writer.write("name: "+p.getName()+
@@ -31,7 +74,7 @@ public class SaveFileManager {
                         "\nmass: "+p.getOrigMass()+
                         "\nspeed: "+p.getSpeed()+
                         "\nangle: "+p.getAngle()+
-                        "\ncolor: "+p.getCurrColor().toString()+
+                        "\ncolor: "+p.getCurrColor().toIntBits()+
                         "\nx: "+p.getxPos()+
                         "\ny: "+p.getyPos()+"\n\n");
             } catch (IOException e) {
