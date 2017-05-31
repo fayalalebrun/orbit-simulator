9fd8926f5a15f6d67648bcd8ff1f59c8adb56de8
AYALA LEBRUN, FRANCISCO <fraayala19@francislalebrun.asfgdmn.local>
2017-05-31 13:58:57 -0500

    Created SaveFileManager class

---
+++ core/src/com/orbit/data/SaveFileManager.java
@@ -0,0 +1,19 @@
+package com.orbit.data;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
+/**
+ * Created by fraayala19 on 5/31/17.
+ */
+public class SaveFileManager {
+
+    public static void loadGame(GameScreen gameScreen, FileHandle file){
+        
+    }
+
+    public static void saveGame(GameScreen gameScreen, FileHandle file){
+
+    }
+
+}
