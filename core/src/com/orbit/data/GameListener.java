0557838c62248a807341f26161b36ba9e6931d87
AYALA LEBRUN, FRANCISCO <fraayala19@francislalebrun.asfgdmn.local>
2017-06-02 10:38:37 -0500

    Removed unnecessary variables

--- core/src/com/orbit/data/GameListener.java
+++ core/src/com/orbit/data/GameListener.java
@@ -27,7 +27,6 @@ public class GameListener extends InputListener implements Disableable{
     boolean placementDisable;
 
     boolean moveInProgress = false;
-    float moveLastX, moveLastY;
 
     OptionsWindow optionsWindow;
 
@@ -52,8 +51,6 @@ public class GameListener extends InputListener implements Disableable{
                 break;
             case MOVE:
                 moveInProgress = true;
-                moveLastX = x;
-                moveLastY = y;
                 return true;
             case ZOOM_IN:
                 zoom(-10);
