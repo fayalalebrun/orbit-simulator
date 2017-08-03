3c435401b353319d79a7ecc77affbf85e8686a3e
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2017-08-02 22:00:45 -0500

    Created abstract class n-body algorithm

---
+++ core/src/com/orbit/data/NBodyAlgorithm.java
@@ -0,0 +1,17 @@
+package com.orbit.data;
+
+import com.orbit.data.entities.Planet;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Fran on 8/2/2017.
+ */
+public abstract class NBodyAlgorithm implements Runnable {
+
+    private ArrayList<Planet> planets;
+
+    public NBodyAlgorithm(ArrayList<Planet> planets) {
+        this.planets = planets;
+    }
+}
