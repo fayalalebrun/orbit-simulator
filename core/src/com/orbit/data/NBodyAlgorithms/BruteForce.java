a31b5ba4890d20f79f51e65fa0093c7d194863b9
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2017-08-03 15:03:30 -0500

    Created the Brute Force class

---
+++ core/src/com/orbit/data/NBodyAlgorithms/BruteForce.java
@@ -0,0 +1,20 @@
+package com.orbit.data.NBodyAlgorithms;
+
+import com.orbit.data.entities.Planet;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Fran on 8/3/2017.
+ */
+public class BruteForce extends NBodyAlgorithm{
+
+    public BruteForce(ArrayList<Planet> planets) {
+        super(planets);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
