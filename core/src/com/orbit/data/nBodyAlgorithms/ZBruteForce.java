44d74354f38f1e9a75b94a4757fd65cbd24bf4e5
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2017-08-05 13:13:03 -0500

    Added ZBruteForce class

---
+++ core/src/com/orbit/data/nBodyAlgorithms/ZBruteForce.java
@@ -0,0 +1,18 @@
+package com.orbit.data.nBodyAlgorithms;
+
+import com.orbit.data.GameScreen;
+
+/**
+ * Created by Fran on 8/5/2017.
+ */
+public class ZBruteForce extends NBodyAlgorithm{
+
+    public ZBruteForce(GameScreen gameScreen) {
+        super(gameScreen);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
