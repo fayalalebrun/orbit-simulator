1fc8cdb8723a86a69798a0e915fea4d0365d0177
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2017-08-06 12:27:45 -0500

    Added v1 at beginning of all existing test cases, to mark deprecated saving format

--- core/src/com/orbit/data/SaveFileManager.java
+++ core/src/com/orbit/data/SaveFileManager.java
@@ -18,56 +18,60 @@ import java.util.Vector;
 public class SaveFileManager {
 
     public static void loadGame(GameScreen gameScreen, FileHandle file){
-        String name;
-        double radius;
-        double mass;
-        double speed;
-        double angle;
-        Color c;
-        double x;
-        double y;
-        float r,g,b,a;
-
         gameScreen.removePlanets(gameScreen.getPlanetArrayList());
 
         String str = file.readString();
         String part[] = str.replaceAll("\\r", "").split("\\n");
-        int i=0;
 
-        while(i<part.length-2){
+        if (part[0].equals("v1")) {
+            String name;
+            double radius;
+            double mass;
+            double speed;
+            double angle;
+            Color c;
+            double x;
+            double y;
+            float r, g, b, a;
 
-            if(part[i].contains("name")){
-                name = part[i].substring(6);
-                i++;
-                radius = Double.parseDouble(part[i].substring(8));
-                i++;
-                mass = Double.parseDouble(part[i].substring(6));
-                i++;
-                speed = Double.parseDouble(part[i].substring(7));
-                i++;
-                angle = Double.parseDouble(part[i].substring(7));
-                i++;
-                r = Float.parseFloat(part[i].substring(10));
-                i++;
-                g = Float.parseFloat(part[i].substring(10));
-                i++;
-                b = Float.parseFloat(part[i].substring(10));
-                i++;
-                a = Float.parseFloat(part[i].substring(10));
-                i++;
 
-                c = new Color(r,g,b,a);
+            int i = 2;
 
-                x = Double.parseDouble(part[i].substring(3));
-                i++;
-                y = Double.parseDouble(part[i].substring(3));
-                i++;
+            while (i < part.length - 2) {
 
-                Planet p = new Planet(name, radius, mass, speed, angle, c, x, y);
+                if (part[i].contains("name")) {
+                    name = part[i].substring(6);
+                    i++;
+                    radius = Double.parseDouble(part[i].substring(8));
+                    i++;
+                    mass = Double.parseDouble(part[i].substring(6));
+                    i++;
+                    speed = Double.parseDouble(part[i].substring(7));
+                    i++;
+                    angle = Double.parseDouble(part[i].substring(7));
+                    i++;
+                    r = Float.parseFloat(part[i].substring(10));
+                    i++;
+                    g = Float.parseFloat(part[i].substring(10));
+                    i++;
+                    b = Float.parseFloat(part[i].substring(10));
+                    i++;
+                    a = Float.parseFloat(part[i].substring(10));
+                    i++;
 
-                gameScreen.addPlanet(p);
-            } else{
-                i++;
+                    c = new Color(r, g, b, a);
+
+                    x = Double.parseDouble(part[i].substring(3));
+                    i++;
+                    y = Double.parseDouble(part[i].substring(3));
+                    i++;
+
+                    Planet p = new Planet(name, radius, mass, speed, angle, c, x, y);
+
+                    gameScreen.addPlanet(p);
+                } else {
+                    i++;
+                }
             }
         }
     }
