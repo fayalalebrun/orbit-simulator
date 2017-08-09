0a12755fe06bd450bf34980e635b392aa62bec46
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2017-08-08 19:49:02 -0500

    Created body for DataAnalysis class

--- core/src/com/orbit/data/dataAnalysis/DataAnalysis.java
+++ core/src/com/orbit/data/dataAnalysis/DataAnalysis.java
@@ -1,7 +1,20 @@
 package com.orbit.data.dataAnalysis;
 
+import com.orbit.data.entities.Planet;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
 /**
  * Created by Fran on 8/8/2017.
  */
 public abstract class DataAnalysis {
+
+    protected Vector<Planet> planetList;
+
+    public DataAnalysis(Vector<Planet> planetList) {
+        this.planetList = planetList;
+    }
+
+    public abstract void run();
 }
