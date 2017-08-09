1cc5be40614b80306174492c2375512ab3d0c260
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2017-08-08 20:34:03 -0500

    Created class for Interval-based analysis

---
+++ core/src/com/orbit/data/dataAnalysis/IntervalPositionAnalysis.java
@@ -0,0 +1,20 @@
+package com.orbit.data.dataAnalysis;
+
+import com.orbit.data.GameScreen;
+import com.orbit.data.entities.Planet;
+
+import java.util.Vector;
+
+/**
+ * Created by Fran on 8/8/2017.
+ */
+public class IntervalPositionAnalysis extends DataAnalysis{
+    public IntervalPositionAnalysis(GameScreen gameScreen) {
+        super(gameScreen);
+    }
+
+    @Override
+    public void run(double delta) {
+
+    }
+}
