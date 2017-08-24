11774c50a474381216fa58ab2ae9c874e998fb6b
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-08-24 11:04:10 -0500

    Created CreditsWindow class

---
+++ core/src/com/orbit/data/UI/CreditsWindow.java
@@ -0,0 +1,37 @@
+package com.orbit.data.UI;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.kotcrab.vis.ui.util.TableUtils;
+import com.kotcrab.vis.ui.widget.VisLabel;
+import com.kotcrab.vis.ui.widget.VisTextButton;
+import com.kotcrab.vis.ui.widget.VisWindow;
+
+/**
+ * Created by fraayala19 on 8/24/17.
+ */
+public class CreditsWindow extends VisWindow{
+    public CreditsWindow(){
+        super("Credits");
+
+        TableUtils.setSpacingDefaults(this);
+        addWidgets();
+        pack();
+    }
+
+    private void addWidgets(){
+        VisLabel label = new VisLabel("Textures  - ");
+        add(label).row();
+        VisTextButton closeButton = new VisTextButton("Close");
+        final CreditsWindow window = this;
+        closeButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                window.setVisible(false);
+            }
+        });
+        this.add(closeButton);
+    }
+}
