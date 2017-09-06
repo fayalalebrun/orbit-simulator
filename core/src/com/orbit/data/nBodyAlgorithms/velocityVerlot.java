45891f6007ae35af6536f14dec5f4d80d7d8ef21
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-09-06 08:04:59 -0500

    Created velocity verlot algorithm class

---
+++ core/src/com/orbit/data/nBodyAlgorithms/velocityVerlot.java
@@ -0,0 +1,17 @@
+package com.orbit.data.nBodyAlgorithms;
+
+import com.orbit.data.GameScreen;
+
+/**
+ * Created by fraayala19 on 9/6/17.
+ */
+public class velocityVerlot extends NBodyAlgorithm {
+    public velocityVerlot(GameScreen gameScreen) {
+        super(gameScreen);
+    }
+
+    @Override
+    protected void runAlgorithm() {
+
+    }
+}
