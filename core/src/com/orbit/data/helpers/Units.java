f35e3d56f6f784df9d4a03744ec3002914e799d0
2019 - ROCKENZAHN GALLEGOS, DAVID THOMAS <davrockenzahn19@David-Rockenzahn-Gallegos.local>
2017-12-08 14:34:15 -0600

    Revert "change gravity to 1"
    This reverts commit 6ebc4b7ed7a6cd6e7262a995aa3922eaae8f2de7.

--- core/src/com/orbit/data/helpers/Units.java
+++ core/src/com/orbit/data/helpers/Units.java
@@ -4,8 +4,7 @@ package com.orbit.data.helpers;
  * Created by Fran on 8/3/2017.
  */
 public class Units {
-    public static final double GRAV = 1;
-            //6.67408 * Math.pow(10,-11);
+    public static final double GRAV = 6.67408 * Math.pow(10,-11);
 
     public static double mToAU(double meters){
         return meters/149597870691.0;
