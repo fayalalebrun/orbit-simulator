299cc4cccc927717ea2deac6827603be76889d00
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2018-01-19 14:22:14 -0600

    Added TutorialWindow class

---
+++ core/src/com/orbit/data/UI/tutorial/TutorialWindow.java
@@ -0,0 +1,27 @@
+package com.orbit.data.UI.tutorial;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.kotcrab.vis.ui.util.TableUtils;
+import com.kotcrab.vis.ui.widget.VisScrollPane;
+import com.kotcrab.vis.ui.widget.VisWindow;
+
+/**
+ * Created by fraayala19 on 1/19/18.
+ */
+public class TutorialWindow extends VisWindow{
+
+
+
+    public TutorialWindow() {
+        super("Tutorial");
+        TableUtils.setSpacingDefaults(this);
+        pack();
+    }
+
+
+
+    private void changeStep(){
+
+    }
+
+}
