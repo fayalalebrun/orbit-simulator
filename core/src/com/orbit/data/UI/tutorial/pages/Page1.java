5ce601bcbf40550579ea95510edeabc902c5c9d9
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2018-01-23 14:43:20 -0600

    Added first page of tutorial

--- core/src/com/orbit/data/UI/tutorial/pages/Page1.java
+++ core/src/com/orbit/data/UI/tutorial/pages/Page1.java
@@ -1,7 +1,11 @@
 package com.orbit.data.UI.tutorial.pages;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.kotcrab.vis.ui.widget.VisImage;
 import com.kotcrab.vis.ui.widget.VisLabel;
+import com.orbit.data.Boot;
 import com.orbit.data.UI.tutorial.TutorialPage;
 import com.orbit.data.UI.tutorial.TutorialWindow;
 
@@ -11,8 +15,8 @@ import com.orbit.data.UI.tutorial.TutorialWindow;
  */
 public class Page1 extends TutorialPage {
 
-    public Page1(TutorialWindow tutorialWindow) {
-        super(tutorialWindow);
+    public Page1(TutorialWindow tutorialWindow, float width) {
+        super(tutorialWindow, width);
     }
 
     @Override
@@ -21,12 +25,43 @@ public class Page1 extends TutorialPage {
     }
 
     @Override
-    protected void build() {
-        VisLabel body = new VisLabel("a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a");
-        body.setWrap(true);
-        add(body).width(175);
+    protected void build(float width) {
+        addText("To get started, click on the File Button at the top left of the screen.", width);
         row();
-        add(previousButton);
-        add(nextButton);
+
+        addImage("tutorial/tutFile.png",width);
+
+        row();
+
+        addText("Now, please select the \"Load\" function.",width);
+        row();
+
+        addImage("tutorial/tutLoad.png",width);
+
+        row();
+
+        addText("Please search for the \"3D_Solar_System_2017_08_06.txt\" test case and select it. If" +
+                "you received the program via DVD, it should be in a folder named \"examples\".",width);
+
+        row();
+
+        addImage("tutorial/tutSelect.png",width);
+
+        row();
+
+        addText("Now click on the button labeled \"Open\".",width);
+
+        row();
+
+        addImage("tutorial/tutOpen.png",width);
+
+        row();
+
+        addText("Congratulations! You have loaded a case.", width);
+
+        row();
+
+        addButtons();
+
     }
 }
