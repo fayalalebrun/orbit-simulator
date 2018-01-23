1f6f7e6df1fbad3b73d5cf71f59ce945377fca49
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2018-01-23 13:17:50 -0600

    Added methods to add functions more easily

--- core/src/com/orbit/data/UI/tutorial/TutorialPage.java
+++ core/src/com/orbit/data/UI/tutorial/TutorialPage.java
@@ -1,11 +1,15 @@
 package com.orbit.data.UI.tutorial;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.kotcrab.vis.ui.widget.VisImage;
+import com.kotcrab.vis.ui.widget.VisLabel;
 import com.kotcrab.vis.ui.widget.VisTable;
 import com.kotcrab.vis.ui.widget.VisTextButton;
+import com.orbit.data.Boot;
 
 /**
  * Created by fraayala19 on 1/19/18.
@@ -70,6 +74,29 @@ public abstract class TutorialPage extends Table {
 
     protected static float calcImageHeight(float width, Image image){
         return (image.getHeight()/image.getWidth())*width;
+    }
+
+    protected void addText(String text, float width){
+        VisLabel body = new VisLabel(text);
+        body.setWrap(true);
+        add(body).width(width).padBottom(5f);
+    }
+
+    protected void addImage(String path, float width){
+        VisImage image = new VisImage(Boot.manager.get(path, Texture.class));
+
+
+        add(image).width(width-50).height(calcImageHeight(width-50,image)).padBottom(8f);
+
+    }
+
+    protected void addButtons(){
+        Table table = new Table();
+        table.left();
+        table.add(previousButton).padRight(3f);
+        table.left();
+        table.add(nextButton);
+        add(table).left();
     }
 
     protected abstract void assignPages();
