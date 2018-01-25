f032697759008b7ea2d5cb8373d146e1ec4f0a8d
2019 - ROCKENZAHN GALLEGOS, DAVID THOMAS <davrockenzahn19@David-Rockenzahn-Gallegos.local>
2018-01-25 17:19:56 -0600

    Added the "help" window

---
+++ core/src/com/orbit/data/UI/tutorial/HelpWindow.java
@@ -0,0 +1,28 @@
+package com.orbit.data.UI.tutorial;/*
+* =====================================================================
+* ==      Created by davrockenzahn19        ==    Date: 1/25/18   ==
+* =====================================================================
+* ==      Project: Orbit Simulator    ==
+* =====================================================================
+
+*/
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.kotcrab.vis.ui.widget.VisWindow;
+
+public class HotkeyWindow extends VisWindow{
+    private static float WIDTH = 400f;
+
+
+    public HotkeyWindow() {
+        super("Hotkeys");
+
+        setVisible(false);
+
+        Table table = new Table();
+
+        super.add()
+
+    }
+
+}
